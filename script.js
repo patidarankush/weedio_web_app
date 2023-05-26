fc209a40dac0cdbce8a92f287150fb1fcbc0911e
patidarankush <patidarankush59@gmail.com>
2023-05-26 07:58:06 +0530

    data page updated

--- script.js
+++ script.js
@@ -19,4 +19,17 @@ document.addEventListener("DOMContentLoaded", () => {
 
   
 
+  // Wrap the event listener in a DOMContentLoaded event handler to ensure the button element is available in the DOM
+document.addEventListener("DOMContentLoaded", () => {
+    // Get the "Start Scan" button element
+    const startScanButton = document.getElementById("sensor-data");
   
+    // Check if the button element exists
+    if (startScanButton) {
+      // Add a click event listener to the button
+      startScanButton.addEventListener("click", () => {
+        // Redirect the user to scan.html
+        window.location.href = "datadisplay.html";
+      });
+    }
+  });
